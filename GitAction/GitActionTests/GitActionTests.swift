13fed6780948f9f5cfe0063fc6713adde6e46f98
park <wogus3602@naver.com>
2020-10-27 01:06:30 +0900

    dasd

--- GitAction/GitActionTests/GitActionTests.swift
+++ GitAction/GitActionTests/GitActionTests.swift
@@ -11,7 +11,6 @@ import XCTest
 class GitActionTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
