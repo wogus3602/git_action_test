a62f6fa6f851ef55c06480989b7c429d9df3b892
park <wogus3602@naver.com>
2020-10-26 20:56:43 +0900

    init

--- GitAction/GitActionTests/GitActionTests.swift
+++ GitAction/GitActionTests/GitActionTests.swift
@@ -26,7 +26,6 @@ class GitActionTests: XCTestCase {
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
 
