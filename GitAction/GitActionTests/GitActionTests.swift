f96c04b75d5c861a1dcdd822753e5b08043eaab0
park <wogus3602@naver.com>
2020-10-30 19:08:21 +0900

    init

--- GitAction/GitActionTests/GitActionTests.swift
+++ GitAction/GitActionTests/GitActionTests.swift
@@ -26,3 +26,13 @@ class GitActionTests: XCTestCase {
     }
 
 }
+
+//
+//with:
+//  github-token: ***
+//  script: await github.pulls.createReview({
+//  ...context.repo,
+//  context.payload.pull_request.number,
+//  body: “👋 테스트코드가 실패했습니다.“,
+//  event: “REQUEST_CHANGES”
+//})
