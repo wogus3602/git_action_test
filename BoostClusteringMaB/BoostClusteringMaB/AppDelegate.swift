df69900442073541858fca6c0606191d71912ec8
park <wogus3602@naver.com>
2020-11-23 19:59:07 +0900

    ads

--- BoostClusteringMaB/BoostClusteringMaB/AppDelegate.swift
+++ BoostClusteringMaB/BoostClusteringMaB/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
+        // Called when a new scene session is bdaseing created.
         // Use this method to select a configuration to create the new scene with.aaa
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
